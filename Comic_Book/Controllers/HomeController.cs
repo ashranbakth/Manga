972cbf700c904ae990877e6db8623e7fe52b70a9
Ashran Bakth <ashranbakth@ashrans-mbp.attlocal.net>
2018-03-04 00:04:21 -0500

    Improved Detail View and added class in the view

--- Comic_Book/Controllers/HomeController.cs
+++ Comic_Book/Controllers/HomeController.cs
@@ -32,8 +32,31 @@ namespace Comic_Book.Controllers
 
         public IActionResult Detail()
         {
+            var manga = new Manga()
+            {
+                id = 1,
+                title = "Naruto",
+                summary = "<p>Twelve years before the start of the series, a powerful " +
+                     "creature known as the Nine-tailed Demon Fox attacked the ninja " +
+                     "village Konohagakure, decimating many people. In response, the leader " +
+                     "of Konohagakure's ninja military - <strong> theFourth Hokage </strong> - sacrificed " +
+                     "his life to seal the demon inside his newly born child Naruto " +
+                     "Uzumaki. Konohagakure, however, regarded Naruto as if he were the demon " +
+                     "fox itself and mistreated him throughout most of his childhood. " +
+                     "The plot tells the story of Naruto Uzumaki, now a adolescent ninja " +
+                     "who constantly searches for recognition and dreams to become the " +
+                     "Hokage, the ninja in his village who is acknowledged as the leader and the strongest of all. </p>", 
+                characters = new string[]{
+                    "Naruto",
+                    "Sasuke",
+                    "Sakura",
+                    "Hinata",
+                },
+                favorite = false
 
-            return View();
+            };
+
+            return View(manga);
         }
 
         public IActionResult Error()
--- Comic_Book/Models/Manga.cs
+++ Comic_Book/Models/Manga.cs
@@ -11,8 +11,19 @@ namespace Comic_Book.Models
         public int id { get; set; }
         public string title { get; set; }
         public string summary { get; set; }
-        public string[] Characters { get; set; }
-        public bool Favorite { get; set; }
+        public string[] characters { get; set; }
+        public bool favorite { get; set; }
+
+        public string CoverImageFilename
+        {
+
+            get
+            {
+                return title.ToLower() + ".png";    
+            }
+
+
+        }
 
     }
 }
